209281977a9c139e282289c7ce06a51e7fdd0f0a
Gabriel Correia <camparicorreia@gmail.com>
2024-01-05 00:20:47 -0300

    Commit the mario game by HTML,CSS and Java script

---
+++ js/script.js
@@ -0,0 +1,40 @@
+const mario = document.querySelector('.mario'); 
+const pipe = document.querySelector('.pipe'); 
+
+const jump = () => { 
+    mario.classList.add('jump'); 
+
+    setTimeout(() => {
+    mario.classList.remove('jump'); 
+    } , 500);
+} 
+
+const loop = setInterval(() => {
+
+    console.log('loop')
+
+    const pipePosition = pipe.offsetLeft; 
+    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', ''); 
+    // usar o metodo number e possivel para converter uma string em numero: Number('xxxx')
+
+    console.log(pipePosition)
+
+    if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) { 
+
+        pipe.style.animation = 'none';
+        pipe.style.left = `${pipePosition}px`; 
+
+        mario.style.animation = 'none'; 
+        mario.style.bottom = `${marioPosition}px`; 
+
+        mario.src= './images/gameovermario.png'; 
+        mario.style.width = '90px'; 
+        mario.style.marginLeft = '50px';
+
+        clearInterval(loop);
+
+    }
+
+} , 10); 
+
+document.addEventListener('keydown', jump); 
